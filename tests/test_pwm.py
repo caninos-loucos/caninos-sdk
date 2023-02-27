670e76bd2ccfcad288f5a320ea129bc58871468d
Geovane Fedrecheski <geonnave@gmail.com>
2023-02-27 14:26:59 -0300

    chore: remove platform-dependent test of pwm

--- tests/test_pwm.py
+++ tests/test_pwm.py
@@ -5,5 +5,3 @@ from caninos_sdk.labrador import Labrador
 
 def test_pwm():
     labrador = Labrador("64", kernel_version=">=4.19.98")
-    labrador.pin3.enable_pwm(alias="motor1", freq=1, duty_cycle=0.5)
-    print("Oi\n")
